071d9da00a4c990fa74a26ef4cf553d53d8235c1
iamnotstatic <staticdev20046@gmail.com>
2021-12-29 22:23:22 +0400

    update pool display interface on Ethereum

--- src/constants/eth.ts
+++ src/constants/eth.ts
@@ -3,20 +3,24 @@ export const ethPools = [
     address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     coingeckoId: 'weth',
     symbol: 'WETH',
+    logo: 'https://cryptologos.cc/logos/ethereum-eth-logo.png?v=014',
   },
   {
     address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
     coingeckoId: 'tether',
     symbol: 'USDT',
+    logo: 'https://cryptologos.cc/logos/tether-usdt-logo.png?v=014',
   },
   {
     address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
     coingeckoId: 'usd-coin',
     symbol: 'USDC',
+    logo: 'https://cryptologos.cc/logos/usd-coin-usdc-logo.png?v=014',
   },
   {
     address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
     coingeckoId: 'dai',
     symbol: 'DAI',
+    logo: 'https://cryptologos.cc/logos/multi-collateral-dai-dai-logo.svg?v=014',
   },
 ];
