5d7a298de0402243879dedc6d095f68310c99803
iamnotstatic <staticdev20046@gmail.com>
2022-10-14 23:30:08 +0400

    installed react-router-dom and registered two routes bsc/eth

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
 import Layout from './components/Layout';
 
 const App = () => {
-  return <Layout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/bsc" element={<Layout />} />
+        <Route path="/eth" element={<Layout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
